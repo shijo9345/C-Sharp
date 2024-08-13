10e722a1aa6671266a175b694e0bcd0d7d0d9e06
Shijo <130920346+shijo9345@users.noreply.github.com>
2024-08-13 22:59:35 +0530

    Alternate-Method

---
+++ TwoIntegerSum1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntegerSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Array elements to Store:");
+            int n = int.Parse(Console.ReadLine());
+            int[] size = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                size[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Enter the Target:");
+            int target = int.Parse(Console.ReadLine());
+            bool val = true;
+
+            int start = 0;
+            int end = n - 1;
+            for (int i = start; i < end; i++)
+            {
+                int sum = size[start] + size[end];
+                if (sum == target)
+                {
+                    Console.WriteLine(start + "," + end);
+                    break;
+                }
+                else if (sum < target)
+                {
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+            if (val == true)
+            {
+                Console.WriteLine("Invalid Target");
+            }
+            Console.ReadLine();
+        }
+    }
+}
