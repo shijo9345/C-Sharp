d8016091ddd93094e7026b22e3c8fd0cbce9772e
Shijo <130920346+shijo9345@users.noreply.github.com>
2024-08-18 23:40:03 +0530

    File

---
+++ SubArraySumEqualsToK.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the target:");
+            int k = int.Parse(Console.ReadLine());
+            int[] arr = { 3, 4, 7, 2, -3, 1, 4, 2 };
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    sum += arr[j];
+                    if (sum == k)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+
+    }
+}
