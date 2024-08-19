eab0221694ef68c915dd2c54db82b10ac38d2843
Shijo <130920346+shijo9345@users.noreply.github.com>
2024-08-19 14:50:31 +0530

    HashMap

---
+++ UniqueNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] num = { 4, 1, 2, 2, 1 };
+
+        int result = Unique(num);
+
+        Console.WriteLine("Unique number is " + result);
+
+        Console.ReadLine();
+    }
+
+    static int Unique(int[] num)
+    {
+        var hm = new Dictionary<int, int>();
+
+        for (int i = 0; i < num.Length; i++)
+        {
+            if (hm.ContainsKey(num[i]))
+            {
+                hm[num[i]]++;
+            }
+            else
+            {
+                hm[num[i]] = 1;
+            }
+        }
+
+        for (int i = 0; i < num.Length; i++)
+        {
+            if (hm[num[i]] == 1)
+            {
+                return num[i];
+            }
+        }
+
+        return 0; // return a default value if no unique number is found
+    }
+}
