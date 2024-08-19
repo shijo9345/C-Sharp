a62346d686da92f856103e2a0455578d9c5144a5
Shijo <130920346+shijo9345@users.noreply.github.com>
2024-08-19 12:08:06 +0530

    Hash-Map

---
+++ SubArraySumEqualsToK - .cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] num = { 1, -1, 0 };
+        int k = 0;
+
+        int result = SubarraySum(num, k);
+
+        Console.WriteLine("The number of subarrays whose sum equals " + k + " is: " + result);
+
+        Console.ReadLine();
+    }
+
+    static int SubarraySum(int[] num, int k)
+    {
+        int sum = 0;
+        int count = 0;
+        var hm = new Dictionary<int, int>();
+
+        for (int i = 0; i < num.Length; i++)
+        {
+            sum += num[i];
+
+            if (sum == k)
+            {
+                count++;
+            }
+
+            if (hm.ContainsKey(sum - k))
+            {
+                count += hm[sum - k];
+            }
+
+            if (hm.ContainsKey(sum))
+            {
+                hm[sum] = hm[sum] + 1;
+            }
+            else
+            {
+                hm[sum] = 1;
+            }
+        }
+
+        return count;
+    }
+}
